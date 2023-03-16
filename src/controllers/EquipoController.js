f604414f08cddf8441d0f10e2b606c24c528ce84
AlejandroUcanT <alejandro.ucan.uady@gmail.com>
2023-03-16 16:53:19 -0600

    Refactorización de BD

--- src/controllers/EquipoController.js
+++ src/controllers/EquipoController.js
@@ -5,227 +5,231 @@ var sequelize = require('../database/BD');
 var Equipo = require('../database/model/Equipo');
 
 controllers.create = async (req,res) => {
-  console.log("\nAgregar Equipo");
-
-  // Parámetros del POST
+  // Parámetros POST
   const { NumInventario, Tipo, Marca, Modelo, NumSerie } = req.body;
-  //Parámetro ID
-  console.log("Número de Inventario: " + NumInventario);
-
-  //INSERT INTO equipos(NUM_INVENTARIO, TIPO, MARCA, MODELO, NUM_SERIE) VALUES('', '', '', '', '');
+  
+  //INSERT INTO equipos(num_inventario, tipo, marca, modelo, num_serie) VALUES('', '', '', '', '');
   const data = await Equipo.create({
-    NUM_INVENTARIO: NumInventario,
-    TIPO: Tipo,
-    MARCA: Marca,
-    MODELO: Modelo,
-    NUM_SERIE: NumSerie
+    num_inventario: NumInventario,
+    tipo: Tipo,
+    marca: Marca,
+    modelo: Modelo,
+    num_serie: NumSerie
   })
-  .then(function(data){
-    console.log("Agregar Equipo =>" + data)
-    return data;
+  .then(function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Equipo agregado exitosamente",
+      data:data
+    });
   })
   .catch(err =>{
-    console.log("Error al Agregar Equipo" + err)
-    return err;
+    res.status(500).json({
+      success:false,
+      message:"Error al agregar equipo",
+      data:err
+    });
   })
-  res.status(200).json({
-    success: true,
-    message:"Equipo agregado",
-    data: data
-  });
 }
 
 controllers.get = async (req, res) => {
-  console.log("\nObtener Equipo");
-
+  console.log(req.params.id);
   //Parámetro ID
   const { id } = req.params;
-  console.log("NumInventario => ", id);
-
-  //SELECT * FROM Equipo WHERE NUM_INVENTARIO='';
+  
+  //SELECT * FROM Equipo WHERE num_inventario='';
   const data = await Equipo.findAll({
-      where: { NUM_INVENTARIO: id }
+      where: { num_inventario: id }
   })
-  .then(function(data){
-    return data;
+  .then(function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Equipo obtenido exitosamente",
+      data:data
+    });
   })
-  .catch(error =>{
-    return error;
+  .catch(err => {
+    res.status(500).json({
+      success:false,
+      message:"Error al obtener equipo",
+      data:err
+    });
   })
-  res.json({
-    success: true,
-    data: data
-  });
 }
 
 controllers.update = async (req, res) => {
-  console.log("\nActualizar Equipo");
-
   //Parámetro ID
   const { id } = req.params;
-  console.log("NumInventario => ", id);
 
   //Parámetros del POST
   const { NumInventario, Tipo, Marca, Modelo, NumSerie } = req.body;
   //UPDATE equipos SET NUM_INVENTARIO='', TIPO='', MARCA='', MODELO='', NUM_SERIE='' WHERE NUM_INVENTARIO='';
   const data = await Equipo.update({
-    NUM_INVENTARIO: NumInventario,
-    TIPO: Tipo,
-    MARCA: Marca,
-    MODELO: Modelo,
-    NUM_SERIE: NumSerie
+    num_inventario: NumInventario,
+    tipo: Tipo,
+    marca: Marca,
+    model: Modelo,
+    num_serie: NumSerie
   },
   {
-    where: { NUM_INVENTARIO: id}
+    where: { num_inventario: id}
   })
-  .then( function(data){
-    console.log("Actualizar Equipo =>" + data)
-    return data;
+  .then( function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Equipo actualizado exitosamente",
+      data:data
+    });
   })
   .catch(error => {
-    console.log("Error al Actualizar Equipo" + err)
-    return error;
+    res.status(500).json({
+      success:true,
+      message:"Error al actualizar equipo",
+      data:err
+    });
   })
-  res.json({
-    success:true,
-    data:data,
-    message:"Datos del Equipo Actualizados"
-  });
 }
 
 controllers.delete = async (req, res) => {
-  console.log("\nEliminar Equipo");
-
   //Parámetro ID
   const { id } = req.body;
-  console.log("NumInventario => ", id);
-
-  //DELETE FROM equipos WHERE NUM_INVENTARIO='';
-  const del = await Equipo.destroy({
-    where: { NUM_INVENTARIO: id}
+  
+  //DELETE FROM equipos WHERE num_inventario='';
+  const data = await Equipo.destroy({
+    where: { num_inventario: id}
   })
-  res.json({
-    success:true,
-    deleted:del,
-    message:"Equipo Eliminado"
-  });
+  .then(function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Equipo eliminado exitosamente",
+      data:data
+    });
+  })
+  .error(err => {
+    res.status(500).json({
+      success:false,
+      message:"Error al eliminar equipo",
+      data:err
+    });
+  })
 }
 
-controllers.list = async(req, res) => {
-  console.log("\nLista de Equipos");
-
+controllers.list = async (req, res) => {
   //SELECT * FROM equipos;
   const data = await Equipo.findAll()
-  .then(function(data){
-    return data;
+  .then(function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Listado de equipos obtenido",
+      data:data
+    });
   })
   .catch(err => {
-    return err;
+    res.status(500).json({
+      success:false,
+      message:"Error al obtener listado de equipos",
+      data:err
+    });
   })
-
-  res.json({
-    success: true,
-    data: data
-  });
 }
 
-controllers.disponible = async(req, res) => {
-  console.log("Lista de Equipos");
-
-  //SELECT * FROM equipos;
+controllers.disponible = async (req, res) => {
+  //SELECT * FROM equipos WHERE disponibilidad = 'Disponible';
   const data = await Equipo.findAll({
     where: {
-      DISPONIBILIDAD: 'Disponible'
+      disponibilidad:'Disponible'
     }
   })
-  .then(function(data){
-    return data;
+  .then(function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Listado de equipos disponibles obtenido",
+      data:data
+    });
   })
   .catch(err => {
-    return err;
+    res.status(500).json({
+      success:false,
+      message:"Error al obtener listado de equipos disponibles",
+      data:err     
+    });
   })
-
-  res.json({
-    success: true,
-    data: data
-  });
 }
 
-controllers.prestado = async(req, res) => {
-  console.log("Lista de Equipos");
-
-  //SELECT * FROM equipos;
+controllers.prestado = async (req, res) => {
+  //SELECT * FROM equipos WHERE disponibilidad = "En Préstamo";
   const data = await Equipo.findAll({
     where: {
-      DISPONIBILIDAD: 'En Préstamo'
+      disponibilidad:'En Préstamo'
     }
-
   })
-  .then(function(data){
-    return data;
+  .then(function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Listado de equipos en préstamo obtenido",
+      data:data
+    });
   })
   .catch(err => {
-    return err;
+    res.status(500).json({
+      success:false,
+      message:"Error al obtener listado de equipos en préstamo",
+      data:err
+    });
   })
-
-  res.json({
-    success: true,
-    data: data
-  });
 }
 
 controllers.prestamo = async (req, res) => {
-  console.log("\nCambiar disponibilidad del Equipo");
-
   const { NumInventario } = req.body;
-  console.log("NumInventario => ", NumInventario);
 
+  //UPDATE equipos SET disponibilidad = "En Préstamo" WHERE num_inventario = '';
   const data = await Equipo.update({
-    DISPONIBILIDAD: 'En Préstamo',
+    disponibilidad:'En Préstamo',
   },
   {
-    where: { NUM_INVENTARIO: NumInventario}
+    where: { num_inventario:NumInventario}
   })
-  .then( function(data){
-    console.log("Cambiar disponibilidad del Equipo =>" + data)
-    return data;
+  .then( function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Préstamo de equipo exitoso",
+      data:data    
+    });
   })
-  .catch(error => {
-    console.log("Error al cambiar disponibilidad del Equipo" + error)
-    return error;
+  .catch(err => {
+    res.status(500).json({
+      success:false,
+      message:"Error al prestar equipo",
+      data:err
+    });
   })
-  res.json({
-    success:true,
-    data:data,
-    message:"Datos del Equipo Actualizados"
-  });
+  
 }
 
 controllers.devolucion = async (req, res) => {
-  console.log("\nCambiar disponibilidad del Equipo");
-
   const { NumInventario } = req.body;
-  console.log("NumInventario => ", NumInventario);
-
+  
+  //UPDATE equipos SET disponibilidad='Disponible' WHERE num_inventario='';
   const data = await Equipo.update({
-    DISPONIBILIDAD: 'Disponible',
+    disponibilidad:'Disponible',
   },
   {
-    where: { NUM_INVENTARIO: NumInventario}
+    where: { num_inventario:NumInventario}
   })
-  .then( function(data){
-    console.log("Cambiar disponibilidad del Equipo =>" + data)
-    return data;
+  .then(function(data) {
+    res.status(200).json({
+      success:true,
+      message:"Devolución de equipo exitoso",
+      data:data
+    });
   })
-  .catch(error => {
-    console.log("Error al cambiar disponibilidad del Equipo" + error)
-    return error;
+  .catch(err => {
+    res.status(500).json({
+      success:false,
+      message:"Error al devolver equipo",
+      data:err
+    });
   })
-  res.json({
-    success:true,
-    data:data,
-    message:"Datos del Equipo Actualizados"
-  });
 }
 
 module.exports = controllers;
--- src/controllers/PrestamoController.js
+++ src/controllers/PrestamoController.js
@@ -8,215 +8,221 @@ const adminConsulta = require('sequelize');
 const op = adminConsulta.Op;
 
 controllers.create = async (req,res) => {
-  console.log("\nAgregar Préstamo");
-
   // Parámetros del POST
   const { IdPrestamo, Solicitante, Procedencia, IdEquipo, FechaPrestamo, FechaDevolucion, TipoPrestamo } = req.body;
 
   //INSERT INTO prestamos(ID_PRESTAMO, SOLICITANTE, PROCEDENCIA, ID_EQUIPO, FECHA_PRESTAMO, HORA_PRESTAMO, FECHA_DEVOLUCION, HORA_DEVOLUCION, TIPO_PRESTAMO VALUES('', '', '', '', '', '', '', '', '', '');
   const data = await Prestamo.create({
-    ID_PRESTAMO: IdPrestamo,
-    SOLICITANTE: Solicitante,
-    PROCEDENCIA: Procedencia,
-    ID_EQUIPO: IdEquipo,
-    FECHA_PRESTAMO: FechaPrestamo,
-    FECHA_DEVOLUCION: FechaDevolucion,
-    TIPO_PRESTAMO: TipoPrestamo
+    id_prestamo: IdPrestamo,
+    solicitante: Solicitante,
+    procedencia: Procedencia,
+    id_equipo: IdEquipo,
+    fecha_prestamo: FechaPrestamo,
+    fecha_devolucion: FechaDevolucion,
+    tipo_prestamo: TipoPrestamo
   })
   .then(function(data){
-    mensaje = 'Préstamo Guardado';
-    return data;
+    res.status(200).json({
+      success:true,
+      message:"Préstamo agregado exitosamente",
+      data:data
+    });
   })
   .catch(err => {
-    mensaje = 'Error'+ err;
-    console.log(err)
-    return err;
+    res.status(500).json({
+      success:false,
+      message:"Error al agregar préstamo",
+      data:err
+    });
   })
-  res.status(200).json({
-    success: true,
-    message: mensaje,
-    data: data
-  });
 }
 
 controllers.get = async (req, res) => {
-  console.log("\nObtener Préstamo");
-
   const { id } = req.params;
-  console.log("ID Préstamo => ", id);
 
   //SELECT * FROM prestamos WHERE ID_PRESTAMO = 'id';
   const data = await Prestamo.findAll({
-      where: { ID_PRESTAMO: id }
+    where: { id_prestamo: id }
   })
   .then(function(data){
-    mensaje = 'Búsqueda realizada';
-    return data;
+    res.status(200).json({
+      success:true,
+      message:"Préstamo obtenido exitosamente",
+      data:data
+    });
   })
   .catch(err => {
-    mensaje = 'Error en la búsqueda';
-    return err;
+    res.status(500).json({
+      success:false,
+      message:"Error al obtener préstamo",
+      data:err
+    });
   })
-  res.status(200).json({
-    success: true,
-    message: mensaje,
-    data: data
-  });
 }
 
 controllers.update = async (req, res) => {
-  console.log("\nActualizar Préstamo");
-
   //Parámetro ID
   const { id } = req.params;
-  console.log("ID del Préstamo: " + id);
-
+  
   // parameter POST
   const { IdPrestamo, IdEquipo, FechaDevolucion, HoraDevolucion } = req.body;
 
   //UPDATE usuarios SET DISPONIBILIDAD='En Prestamo' WHERE NUM_INVENTARIO ='';
   const data = await Prestamo.update({
-    ID_PRESTAMO: IdPrestamo,
-    ID_EQUIPO: IdEquipo,
-    FECHA_DEVOLUCION: FechaDevolucion,
-    HORA_DEVOLUCION: HoraDevolucion
+    id_prestamo: IdPrestamo,
+    id_equipo: IdEquipo,
+    fecha_devolucion: FechaDevolucion,
+    hora_devolucion: HoraDevolucion
   },
   {
-    where: { ID_PRESTAMO: id}
+    where: { id_prestamo: id}
   })
-  .then( function(data){
-    console.log("Actualizar Préstamo =>" + data)
-    return data;
+  .then(function(data){
+    res.status(200).json({
+      success:true,
+      message:"Actualización de préstamo exitoso",
+      data:data
+    });
   })
   .catch(error => {
-    console.log("Error al Actualizar Préstamo" + err)
-    return error;
+    res.status(500).json({
+      success:false,
+      message:"Error al actualizar préstamo",
+      data:err
+    });
   })
-  res.json({
-    success:true,
-    data:data
-  });
 }
 
 controllers.delete = async (req, res) => {
-  console.log("\nEliminar prestamo");
-
-  // parameter post
+  // parameter POST
   const { id } = req.body;
 
-  console.log("ID prestamo => ", id);
   // delete sequelize
-  const del = await Prestamo.destroy({
-    where: { ID_PRESTAMO: id}
+  const data = await Prestamo.destroy({
+    where: { id_prestamo: id}
   })
-  res.json({
-    success:true,
-    deleted:del,
-    message:"Préstamo Cancelado"
-  });
+  .then(function(data){
+    res.status(200).json({
+      success:true,
+      message:"Eliminación de préstamo exitoso",
+      data:data
+    });
+  })
+  .catch(error => {
+    res.status(500).json({
+      success:false,
+      message:"Error al eliminar préstamo",
+      data:err
+    });
+  })
 }
 
 controllers.rango = async (req, res) => {
-  console.log("\nConsulta Préstamo");
-
   // parameter POST
   const { FechaInicial, FechaCorte } = req.body;
-  console.log("Fecha inicio", FechaInicial)
-  console.log("Fecha corte", FechaCorte)
-
+  
   //UPDATE usuarios SET DISPONIBILIDAD='En Prestamo' WHERE NUM_INVENTARIO ='';
   const data = await Prestamo.findAll({
     where: {
-      FECHA_PRESTAMO: {
+      fecha_prestamo: {
         [op.between]: [FechaInicial , FechaCorte]
       }
     }
   })
-  .then( function(data){
-    console.log("Consulta Préstamo =>" + data)
-    return data;
+  .then(function(data){
+    res.status(200).json({
+      success:true,
+      message:"Obtención de préstamos en rango exitoso",
+      data:data
+    });
   })
   .catch(error => {
-    console.log("Error al Consultar Préstamo" + err)
-    return error;
+    res.status(500).json({
+      success:false,
+      message:"Error al obtener lista de préstamos por rango",
+      data:err
+    });
   })
-  res.json({
-    success:true,
-    data:data
-  });
 }
 
 controllers.filtro = async (req, res) => {
-  console.log("\nConsulta Préstamo Filtro");
-
   // parameter POST
   const { Filtro, Argumento } = req.body;
-  console.log("Filtro =>", Filtro);
-  console.log("Argumento =>", Argumento);
 
   //UPDATE usuarios SET DISPONIBILIDAD='En Prestamo' WHERE NUM_INVENTARIO ='';
   switch(Filtro){
     case 'PROCEDENCIA':
       const dataP = await Prestamo.findAll({
         where: {
-          PROCEDENCIA: Argumento
+          procedencia: Argumento
         }
       })
-      .then( function(dataP){
-        console.log("Consulta Préstamo =>" + dataP)
-        return dataP;
+      .then(function(data){
+        res.status(200).json({
+          success:true,
+          message:"Obtención de préstamos por procedencia",
+          data:data
+        });
       })
       .catch(error => {
-        console.log("Error al Consultar Préstamo" + error)
-        return error;
+        res.status(500).json({
+          success:false,
+          message:"Error al obtener lista de préstamos por procedencia",
+          data:err
+        });
       })
-      res.json({
-        success: true,
-        data: dataP
-      });
       break;
 
     case 'EQUIPO':
       const dataE = await Prestamo.findAll({
         where: {
-          EQUIPO: Argumento
+          equipo: Argumento
         }
       })
-      .then( function(dataE){
-        console.log("Consulta Préstamo =>" + dataE)
-        return dataE;
+      .then(function(data){
+        res.status(200).json({
+          success:true,
+          message:"Obtención de préstamos por tipo de equipo",
+          data:data
+        });
       })
       .catch(error => {
-        console.log("Error al Consultar Préstamo" + error)
-        return error;
+        res.status(500).json({
+          success:false,
+          message:"Error al obtener lista de préstamos por tipo de equipo",
+          data:err
+        });
       })
-      res.json({
-        success: true,
-        data: dataE
-      });
       break;
 
     case 'TIPO_PRESTAMO':
       const dataT = await Prestamo.findAll({
         where: {
-          TIPO_PRESTAMO: Argumento
+          tipo_prestamo: Argumento
         }
       })
-      .then( function(dataT){
-        console.log("Consulta Préstamo =>" + dataT)
-        return dataT;
+      .then(function(data){
+        res.status(200).json({
+          success:true,
+          message:"Obtención de préstamos por tipo de préstamo",
+          data:data
+        });
       })
       .catch(error => {
-        console.log("Error al Consultar Préstamo" + error)
-        return error;
+        res.status(500).json({
+          success:false,
+          message:"Error al obtener lista de préstamos por tipo de préstamo",
+          data:err
+        });
       })
-      res.json({
-        success: true,
-        data: dataT
-      });
       break;
 
-    default: console.log('Sin Filtro')
+    default: 
+      res.status(500).json({
+        success:false,
+        message:"No se ha especificado el filtro",
+        data:null
+      });
   }
 }
 
